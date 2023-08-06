9143013714f618a05274a4fa735b0a40ac37d086
mdmubin <mdmubin.xyz@gmail.com>
2023-08-07 03:01:16 +0600

    fixed token set to null

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,25 +1,27 @@
 /* eslint-disable no-underscore-dangle */
 
+import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
+import * as config from '../config/config.js';
 import { throwServerError } from './utils.js';
 
 export function authenticateUser(req, res, next) {
-  const authenticate = async (rq, rs) => {
-    const { email, password } = rq.body;
+  const authenticate = async () => {
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (user && (await user.verifyPassword(password))) {
-      rs.json({
+      res.json({
         _id: user._id,
         isAdmin: user.isAdmin,
-        token: null,
+        token: jwt.sign({ _id: user._id }, config.JWT_SECRET, { expiresIn: '30d' }),
       });
     } else {
       throwServerError('invalid username / password', 401);
     }
   };
 
-  Promise.resolve(authenticate(req, res))
+  Promise.resolve(authenticate())
     .catch((error) => next(error));
 }
 
